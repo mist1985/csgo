c80758b3068285f0459dd246b7b94ab599d06ea9
mist1985 <mihajlo.stojanovski@yahoo.com>
2024-01-20 18:22:49 +0100

    update

--- pages/homepage.ts
+++ pages/homepage.ts
@@ -1,4 +1,5 @@
 import { expect, Locator, Page } from '@playwright/test';
+import { error } from 'console';
 
 
 export class HomePage {
@@ -24,6 +25,14 @@ readonly empireFooter: Locator;
 readonly supportBtn: Locator;
 readonly bet100: Locator;
 readonly clearBtn: Locator;
+readonly headerFreecase: Locator;
+readonly headerFairness: Locator;
+readonly headerBonusCases: Locator;
+readonly headerReferrals: Locator;
+readonly headerAbout: Locator;
+readonly headerDailyCoins: Locator;
+readonly popupSignIn: Locator;
+readonly popupClose: Locator;
 
 
 ///-------------------------------------------------- SELECTORS -------------------------------------------------- ///
@@ -31,7 +40,7 @@ readonly clearBtn: Locator;
 
 constructor(page: Page) {
     this.page = page;
-    this.logo = page.locator(`//*[@id="empire-header"]/div[1]/div/div[1]`);
+    this.logo = page.locator(`div.logo.flex.items-center.justify-center`);
     this.roulette = page.locator('//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div/div[2]');
     this.previousRolls = page.locator(`//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div/div[3]/div/div[1]/div[1]`);
     this.last100 = page.locator(`//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div/div[3]/div/div[2]`);
@@ -44,13 +53,22 @@ constructor(page: Page) {
     this.chatHeader = page.locator(`//*[@id="popover-reference"]/button`)
     this.empireHeader = page.locator(`//*[@id="empire-header"]/div[1]/div/div[2]/div`)
     this.subMenu = page.locator(`//*[@id="empire-header"]/div[1]/div/div[3]/div[1]`);
-    this.withdrawButton = page.locator(`//*[@id="empire-header"]/div[1]/div/div[3]/div[2]/div[1]/a/button/span`);
-    this.depositButton = page.locator(`//*[@id="empire-header"]/div[1]/div/div[3]/div[2]/div[2]/a/button/span`);
-    this.signInButton = page.locator(`//*[@id="empire-header"]/div[1]/div/div[3]/div[2]/div[3]/button/span/div[2]`);
+    this.withdrawButton = page.getByRole('button', { name: 'Withdraw' }).first();
+    this.depositButton = page.getByRole('button', { name: 'Deposit' }).first();
+    this.signInButton = page.locator(`//*[@id="empire-header"]/div[1]/div/div[3]/div[2]/div[3]/button`)
     this.empireFooter = page.locator(`//*[@id="empire-footer"]/div/div`);
     this.supportBtn = page.locator(`//*[@id="app"]/button/div`);
-    this.bet100 = page.locator(`//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div/div[4]/div/div[2]/div/button[6]`)
+    this.bet100 = page.getByRole('button', { name: '+ 100' });
     this.clearBtn = page.getByRole('button', { name: 'Clear' });
+    this.headerFreecase = page.locator('a[href="#free-case"]').first();
+    this.headerFairness = page.locator('a[href="/fairness"]').first();
+    this.headerBonusCases = page.locator('a[href="/profile/cases"]').first();
+    this.headerReferrals = page.locator('a[href="/referrals"]').first();
+    this.headerAbout = page.locator('a[href="/faq"]').first();
+    this.headerDailyCoins = page.locator('a[href="#daily-coins"]').first();
+    this.popupSignIn = page.locator('//*[@id="app"]/div[9]/div/div[2]/div/div');
+    this.popupClose= page.locator(`//body/div[@id='app']/div[@id='app']/div[9]/div[1]/div[2]/div[1]/div[1]/div[1]/button[1]/span[1]/div[2]`);
+
   }
 
   async enterBet(bet: number) {
@@ -58,7 +76,7 @@ constructor(page: Page) {
     await this.betInputField.fill(bet.toString());
   }
 
-  async clearBet(){
+    async clearBet(){
     await this.clearBtn.click();
   }
 }
--- tests/home-page-test.spec.ts
+++ tests/home-page-test.spec.ts
@@ -1,27 +1,56 @@
 import { test, Locator, Page, expect } from '@playwright/test';
 import { HomePage } from '../pages/homepage';   
 
+test.beforeEach(async ({ page }, testInfo) => {
+ await page.goto('https://csgoempire.com/roulette');
+ await page.waitForLoadState();
+});
 
 test.describe('Testing the Home Page', () => {
       
     test('Check if home page elements are displayed', async ({ page }) => {
       const homePage = new HomePage(page);
-  
-      await test.step('Go to home page', async () => {
-        await page.goto('https://csgoempire.com/roulette');
-        await page.waitForLoadState();
-      });
-  
+
       await test.step('Check if elements are visble', async () => {
-        await expect(homePage.roulette).toBeVisible();
-        await expect(homePage.previousRolls).toBeVisible();
-        await expect(homePage.last100).toBeVisible();
-        await expect(homePage.betInputField).toBeVisible();
-        await expect(homePage.betButtons).toBeVisible();
-        await expect(homePage.betContainers).toBeVisible();
-        await expect(homePage.dailyRoutineRaceHeader).toBeVisible();
-        await expect(homePage.dailyRoutineRaceTable).toBeVisible();
+        try {
+          await expect(homePage.logo).toBeVisible();
+          await expect(homePage.roulette).toBeVisible();
+          await expect(homePage.previousRolls).toBeVisible();
+          await expect(homePage.last100).toBeVisible();
+          await expect(homePage.betInputField).toBeVisible();
+          await expect(homePage.betButtons).toBeVisible();
+          await expect(homePage.betContainers).toBeVisible();
+          await expect(homePage.dailyRoutineRaceHeader).toBeVisible();
+          await expect(homePage.dailyRoutineRaceTable).toBeVisible();
+          await expect(homePage.chat).toBeVisible();
+          await expect(homePage.chatHeader).toBeVisible();
+          await expect(homePage.empireHeader).toBeVisible();
+          await expect(homePage.subMenu).toBeVisible();
+          await expect(homePage.withdrawButton).toBeVisible();
+          await expect(homePage.depositButton).toBeVisible();
+          await expect(homePage.signInButton).toBeVisible();
+          await expect(homePage.empireFooter).toBeVisible();
+          await expect(homePage.supportBtn).toBeVisible();
+          await expect(homePage.bet100).toBeVisible();
+          await expect(homePage.clearBtn).toBeVisible();
+          await expect(homePage.headerFreecase).toBeVisible();
+          await expect(homePage.headerFairness).toBeVisible();
+          await expect(homePage.headerBonusCases).toBeVisible();
+          await expect(homePage.headerReferrals).toBeVisible();
+          await expect(homePage.headerAbout).toBeVisible();
+          await expect(homePage.headerDailyCoins).toBeVisible();
+    
+        }
+        catch (e){
+          throw new Error(`Not all elements are visible or an error occurred in visibility check. The error message is: ${e}`);
+        }
       });
+      
+    });
+
+    
+    test('Check entering bet works', async ({ page }) => {
+      const homePage = new HomePage(page);
 
       await test.step('Enter a bet, increase it by 100 and clear it ', async () => {
         await homePage.enterBet(500);
@@ -29,5 +58,17 @@ test.describe('Testing the Home Page', () => {
         await homePage.clearBet()
       });
      
+    });
+
+    test('Clicking on Sign In navigates to Steam', async ({ page }) => {
+      const homePage = new HomePage(page);
+
+      await test.step('Click on the Sign in button ', async () => {
+        await homePage.signInButton.click()
+      });
+      await test.step('Steam login page is displayed', async () => {
+        await expect(page).toHaveURL(/https:\/\/steamcommunity\.com\/openid\/loginform\/\?.*/);
+      });
+      
     });
   });
